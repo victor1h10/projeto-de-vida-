43e708a6dd4d66f856bc07a6b88e9a0bed087df1
victor1h10 <victor.morais.assis@escola.pr.gov.br>
2026-04-30 07:40:41 -0300

    Update main.js

--- main.js
+++ main.js
@@ -15,8 +15,8 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2025-10-05T00:00:00");
-const tempoObjetivo2 = new Date("2025-12-05T00:00:00");
+const tempoObjetivo1 = new Date("2026-12-19T00:00:00");
+const tempoObjetivo2 = new Date("2025currículo--T00:00:00");
 const tempoObjetivo3 = new Date("2025-12-30T00:00:00");
 const tempoObjetivo4 = new Date("2025-02-01T00:00:00");
 
